181f3feb2ab190d40bea5f6e49d4334a20184340
Aryan Kumar Singh <aryankumarsingh1503@gmail.com>
2024-04-02 00:48:42 +0530

    yo

--- src/main/java/com/example/runner/run/RunController.java
+++ src/main/java/com/example/runner/run/RunController.java
@@ -1,6 +1,7 @@
 package com.example.runner.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,14 +29,9 @@ public class RunController {
     }
 
     @GetMapping(value="/{id}")
-    public String x()
+    Run findById(@PathVariable Integer id)
     {
-        return "hello";
-    }
-    @GetMapping(value="/1")
-    Run findById()
-    {
-        return runRepository.findById(1);
+        return runRepository.findById(id);
     }
 
 }
